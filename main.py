a89ea0261727c37166559ddf3ff0e907abb966b5
Leonardo Augusto <leosaugusto98@gmail.com>
2023-02-19 23:51:05 -0300

    add gui examples

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from tkinter import *
+from tkinter import ttk
+
+defaultFont='futura'
 
 window = Tk()
+window.title('Network Manager')
+window.geometry('620x620')
+
+frame = ttk.Frame(window, padding=10, relief=SUNKEN)
+frame.grid()
+
+deviceInfoLabel = Label(frame, text='testLabel', font=defaultFont, )
+deviceInfoLabel.grid(row=0,column=0)
+
 window.mainloop()
