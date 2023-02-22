2b5563008c7bfc8a3e1eb1887ba5978c95d975e3
Ghilga <leosaugusto98@gmail.com>
2023-02-21 22:09:26 -0300

    split files by classes and implement exception when starting snmp session

---
+++ SNMPClient.py
@@ -0,0 +1,17 @@
+# from easysnmp import Session
+from tkinter import messagebox
+
+class SNMPClient():
+    def __init__(self, ip):
+        self.clientIp = ip
+        self.clientSession = self.initiateSNMPSession(ip)
+    
+    def initiateSNMPSession(self, ip):
+        #TODO: start SNMPv3 session and return any exception found to show the error
+        try:
+            raise Exception('Timeout')
+        except Exception as ex:
+            self.showIpErrorMessage(ex)
+       
+    def showIpErrorMessage(self, message='This IP is invalid'):
+        messagebox.showerror(title='Error', message=message)
